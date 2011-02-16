60f95958a89cbc820c9d241d5d0cc020846edb8b
Reid Lynch <reid.lynch@gmail.com>
2011-02-16 09:07:45 -0500

    ruby 1.6 compat fix breaks things in real ruby versions

--- lib/action_web_service/soap/property.rb
+++ lib/action_web_service/soap/property.rb
@@ -320,14 +320,14 @@ end
 
 
 # for ruby/1.6.
-unless Enumerable.instance_methods.include?('inject')
-  module Enumerable
-    def inject(init)
-      result = init
-      each do |item|
-	result = yield(result, item)
-      end
-      result
-    end
-  end
-end
+# unless Enumerable.instance_methods.include?('inject')
+#   module Enumerable
+#     def inject(init)
+#       result = init
+#       each do |item|
+#   result = yield(result, item)
+#       end
+#       result
+#     end
+#   end
+# end
